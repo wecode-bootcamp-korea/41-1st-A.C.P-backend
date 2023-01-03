6c8ec37854be0f2848e6a59e814dd71dd8ba7290
minkyu_lee <0106995@naver.com>
2023-01-03 13:11:32 +0900

    Modify: 캐쉬 삭제 및 init 설정에 불필요한 코드 삭제

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
-const routes = require("./routes");
-
-const dotenv = require("dotenv");
-dotenv.config();
+// const routes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
-app.use(routes);
+// app.use(routes);
 app.use(cors());
 app.use(morgan("dev"));
+
+const { DataSource } = require("typeorm");
+
+const appDataSource = new DataSource({
+  type: process.env.TYPEORM_CONNECTION,
+  host: process.env.TYPEORM_HOST,
+  port: process.env.TYPEORM_PORT,
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE,
+});
+
+module.exports = {
+  appDataSource,
+};
+
 
 app.get("/ping", (req, res) => {
   return res.status(200).json({ message: "pong" });
@@ -25,5 +41,14 @@ const start = async () => {
     console.log(err);
   }
 };
+
+appDataSource
+  .initialize()
+  .then(() => {
+    console.log("Data Source has been initialized!");
+  })
+  .catch(() => {
+    console.log("Promise Rejected!");
+  });
 
 start();
--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const userRouter = require("./userRouter");
-
-router.use("/users", userRouter.router);
-
-module.exports = router;
