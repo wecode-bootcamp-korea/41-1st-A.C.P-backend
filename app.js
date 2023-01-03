e50e6f1ffe58e7da53fa1ab6a0f1fc85fba50ba7
minkyu_lee <0106995@naver.com>
2023-01-03 17:22:15 +0900

    modify: 주석처리 삭제

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ dotenv.config();
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
-// const routes = require("./routes");
 
 const { appDataSource } = require("./models/dbconfig");
 
@@ -12,7 +11,6 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
-// app.use(routes);
 app.use(cors());
 app.use(morgan("dev"));
 
