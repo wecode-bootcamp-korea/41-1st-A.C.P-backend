6245c9a06e202ee14d148b9429d77477b13c3cb6
minkyu_lee <0106995@naver.com>
2023-01-05 18:25:32 +0900

    Modify: add pots table option

--- models/potsDao.js
+++ models/potsDao.js
@@ -7,11 +7,14 @@ const potsDetails = async (potsId) => {
                 pots.name,
                 pots.price,
                 pots_potsColors.potsColor_id,
-                potsing.img_url
+                potsing.img_url,
+                potsSizes.size
             FROM
                 pots
             JOIN pots_potsColors ON pots.potsColors.id = pots.id,
-            JOIN potsColors ON potsColors.id = pots_potsColors.potsColors_id, 
+            JOIN potsColors ON potsColors.id = pots_potsColors.potsColors_id,
+            JOIN potsimg ON pots.id = potsimg.pots_id,
+            JOIN potsSizes ON pots.potsSizes_id = potsSizes.id
             WHERE pots.id = ?;
             `,
       [potsId]
