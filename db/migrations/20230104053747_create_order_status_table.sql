eb26f8f93b5ba7964bd9a85ce9ac5b64663ad954
Kwang-Hwi Kim <kkh940227@gmail.com>
2023-01-06 16:17:38 +0900

    MODIFY: migrations 수정

--- db/migrations/20230104053747_create_order_status_table.sql
+++ db/migrations/20230104053747_create_order_status_table.sql
@@ -1,10 +1,10 @@
 -- migrate:up
-CREATE TABLE orderstatus (
+CREATE TABLE order_status (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   type VARCHAR(200) NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT NOW(),
   updated_at TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP,
-  CONSTRAINT orderstatus_type_ukey UNIQUE (type)
+  CONSTRAINT order_status_type_ukey UNIQUE (type)
 );
 -- migrate:down
-DROP TABLE orderstatus;
+DROP TABLE order_status;
--- db/migrations/20230104053806_create_orders_table.sql
+++ db/migrations/20230104053806_create_orders_table.sql
@@ -4,11 +4,11 @@ CREATE TABLE orders (
   uuid INT NOT NULL,
   total_price DECIMAL(10,3) NOT NULL,
   users_id INT NOT NULL,
-  orderstatus_id INT NOT NULL,
+  order_status_id INT NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT NOW(),
   updated_at TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP,
   CONSTRAINT orders_users_id FOREIGN KEY (users_id) REFERENCES users(id),
-  CONSTRAINT orders_orderstatus_id FOREIGN KEY (orderstatus_id) REFERENCES ordersStatus(id)
+  CONSTRAINT orders_order_status_id FOREIGN KEY (order_status_id) REFERENCES order_status(id)
 );
 -- migrate:down
 DROP TABLE orders;
