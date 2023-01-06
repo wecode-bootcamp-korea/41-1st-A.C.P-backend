98f5f67d15b87568239d39297b8bcc17aaa4e839
Kwang-Hwi Kim <kkh940227@gmail.com>
2023-01-06 12:59:27 +0900

    MODIFY: migrations

--- db/migrations/20230102023729_create_users_table.sql
+++ db/migrations/20230102023729_create_users_table.sql
@@ -6,11 +6,11 @@ CREATE TABLE users (
   name VARCHAR(30) NOT NULL,
   phone_number VARCHAR(30) NOT NULL,
   address VARCHAR(200) NULL,
-  point INT NOT NULL DEFAULT 0,
+  point DECIMAL(10,3) NOT NULL DEFAULT 0,
   created_at TIMESTAMP NOT NULL DEFAULT NOW(),
   updated_at TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP,
   CONSTRAINT users_ukey UNIQUE (email),
-  CONSTRAINT phoneNumber_ukey UNIQUE (phoneNumber)
+  CONSTRAINT phone_number_ukey UNIQUE (phone_number)
 );
 -- migrate:down
 DROP TABLE users;
--- db/migrations/20230104042228_create_plantsimg_table.sql
+++ db/migrations/20230104042228_create_plantsimg_table.sql
@@ -1,11 +1,9 @@
 -- migrate:up
-CREATE TABLE plantsimg (
+CREATE TABLE plant_images (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  imgurl VARCHAR(200) NOT NULL,
+  img_url VARCHAR(1000) NOT NULL,
   plants_id INT NOT NULL,
-  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-  updated_at TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP,
-  CONSTRAINT plantsimg_plants_id FOREIGN KEY (plants_id) REFERENCES plants(id)
+  CONSTRAINT plant_images_plants_id FOREIGN KEY (plants_id) REFERENCES plants(id)
 );
 -- migrate:down
-DROP TABLE plantsimg;
+DROP TABLE plant_images;
