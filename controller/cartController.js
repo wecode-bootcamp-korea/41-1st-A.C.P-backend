7a9e7b6c9493347a320e7bd4d5735487d4cf17b8
minkyu_lee <0106995@naver.com>
2023-01-08 17:49:29 +0900

    아몰랑

--- controller/cartController.js
+++ controller/cartController.js
@@ -1,34 +1,7 @@
 const { catchAsync } = require("../utils/error");
 const cartService = require("../services/cartService");
 
-const insertData = catchAsync(async (req, res) => {
-  const {
-    userId,
-    plantId,
-    plantQuantity,
-    potId,
-    potQuantity,
-    nutrientId,
-    nutrientQuantity,
-  } = req.body;
-
-  if (!userId) {
-    throw new Error("KEY_ERROR");
-  }
-
-  await cartService.insertData(
-    userId,
-    plantId,
-    plantQuantity,
-    potId,
-    potQuantity,
-    nutrientId,
-    nutrientQuantity
-  );
-
-  res.status(201).json({ message: "upsert data to cart success!!" });
-});
-
+const getCartList = async(catchAsync(req,res) 
 module.exports = {
   insertData,
 };
