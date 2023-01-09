daebe4283cd21177b230d41bb82c4c084b0b3924
minkyu_lee <0106995@naver.com>
2023-01-09 14:59:34 +0900

    modifing

--- services/userService.js
+++ services/userService.js
@@ -6,15 +6,13 @@ const userDao = require("../models/userDao");
 
 const signIn = async (email, password) => {
   const user = await userDao.getUserEmail(email);
-  console.log(user);
-  if (user.length == 0) throw new Error("INVALID_USER_ID");
+  if (user.length == 0) throw new Error("USER_NOT_FOUND");
 
   const userId = await userDao.getUserId(email);
 
   const hashedPassword = await userDao.getHashedPassword(email);
-  console.log(hashedPassword.hashedPassword);
   const result = await bcrypt.compare(password, hashedPassword.hashedPassword);
-  console.log(result);
+
   if (!result) throw new Error("PASSWORD_DOES_NOT_MATCH");
 
   return jwt.sign(userId, process.env.secretKey);
