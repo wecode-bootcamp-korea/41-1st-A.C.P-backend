673b0eee40f7d5ba0556dfca4d429b35d84f2547
Kwang-Hwi Kim <kkh940227@gmail.com>
2023-01-09 11:28:29 +0900

    MODIFY: pots_pot_colors, carts, order_products

--- db/migrations/20230104051130_create_pots_pot_colors_table.sql
+++ db/migrations/20230104051130_create_pots_pot_colors_table.sql
@@ -3,7 +3,6 @@ CREATE TABLE pots_pot_colors (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   pot_id INT NOT NULL,
   pot_color_id INT NOT NULL,
-  plant_id INT NOT NULL,
   CONSTRAINT pots_pot_colors_pot_id FOREIGN KEY (pot_id) REFERENCES pots(id),
   CONSTRAINT pots_pot_colors_pot_color_id FOREIGN KEY (pot_color_id) REFERENCES pot_colors(id),
   CONSTRAINT pots_pot_colors_plant_id FOREIGN KEY (plant_id) REFERENCES plants(id)
