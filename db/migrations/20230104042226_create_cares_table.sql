44509bf8b69373f7bfe45b92f515d0ad9031d688
minkyu_lee <0106995@naver.com>
2023-01-11 22:20:27 +0900

    DELETED CACHED FROM DB

--- db/migrations/20230104042226_create_cares_table.sql
+++
@@ -1,7 +0,0 @@
--- migrate:up
-CREATE TABLE cares (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL
-);
--- migrate:down
-DROP TABLE cares;
--- db/migrations/20230104051118_create_pot_colors_table.sql
+++
@@ -1,8 +0,0 @@
--- migrate:up
-CREATE TABLE pot_colors (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  color VARCHAR(30) NOT NULL,
-  CONSTRAINT pot_colors_coler_ukey UNIQUE (color)
-);
--- migrate:down
-DROP TABLE pot_colors;
