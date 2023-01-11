f9f2b345e865c9860648a8e8bc1c04a62e6f7332
minkyu_lee <0106995@naver.com>
2023-01-11 12:55:28 +0900

    wip

--- services/userService.js
+++ services/userService.js
@@ -1,24 +1,40 @@
-const bcrypt = require("bcrypt");
-const response = require("express");
-const jwt = require("jsonwebtoken");
+const jwt = require("jsonWebtoken");
 
 const userDao = require("../models/userDao");
 
-const signIn = async (email, password) => {
-  const hashedPassword = await userDao.getHashedPassword(email);
-  const compare = await bcrypt.compare(password, hashedPassword);
+const loginRequired = async (req, res, next) => {
+  const accessToken = req.headers.authorization;
 
-  if (!compare) {
-    const err = new Error("PASSWORD_DOES_NOT_MATCH");
+  if (!accessToken) {
+    const err = new Error("NEED_ACCESS_TOKEN");
     err.statusCode = 401;
-    throw err;
+
+    return res.status(err.statusCode).json({ message: err.message });
   }
 
-  const userId = await userDao.getUserId(email);
+  let decoded;
+  try {
+    decoded = await jwt.verify(accessToken, process.env.JWT_SECRET);
+  } catch {
+    const err = new Error("INVALID TOKEN");
+    err.statusCode = 401;
 
-  return jwt.sign(userId, process.env.secretKey);
+    return res.status(err.statusCode).json({ message: err.message });
+  }
+
+  const user = await userDao.getUserById(decoded.userId);
+
+  if (!user) {
+    const err = new Error("USER_DOES_NOT_MATCH");
+    err.statusCode = 401;
+
+    return res.status(err.statusCode).json({ message: err.message });
+  }
+
+  req.user = user;
+
+  next();
 };
-
 module.exports = {
-  signIn,
+  loginRequired,
 };
