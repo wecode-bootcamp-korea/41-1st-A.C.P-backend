2aed05965df949beb18008697dc36d3532a01e9d
minkyu_lee <0106995@naver.com>
2023-01-11 12:53:19 +0900

    api 초기세팅

---
+++ models/orderDao.js
@@ -0,0 +1,5 @@
+const {appDataSource} = require("./dbconfig");
+
+const orderList = async(
+    
+)
--- utils/auth.js
+++ utils/auth.js
@@ -1,26 +1,34 @@
 const jwt = require("jsonWebtoken");
 
-const { userService } = require("../services/userService");
+const userDao = require("../models/userDao");
 
-const loginRequired = async (req, res) => {
+const loginRequired = async (req, res, next) => {
   const accessToken = req.headers.authorization;
 
   if (!accessToken) {
-    const error = new Error("NEED_ACCESS_TOKEN");
+    const err = new Error("NEED_ACCESS_TOKEN");
     err.statusCode = 401;
 
-    return res.status(error.statusCode).json({ message: error.message });
+    return res.status(err.statusCode).json({ message: err.message });
   }
 
-  const decoded = await jwt.verify(accessToken, process.env.JWT_SECRET);
-
-  const user = await userService.getUserById(decoded.id);
-
-  if (!user) {
-    const error = new Error("USER_DOES_NOT_MATCH");
+  let decoded;
+  try {
+    decoded = await jwt.verify(accessToken, process.env.JWT_SECRET);
+  } catch {
+    const err = new Error("INVALID TOKEN");
     err.statusCode = 401;
 
-    return res.status(error.statusCode).json({ message: error.message });
+    return res.status(err.statusCode).json({ message: err.message });
+  }
+
+  const user = await userDao.getUserById(decoded.userId);
+
+  if (!user) {
+    const err = new Error("USER_DOES_NOT_MATCH");
+    err.statusCode = 401;
+
+    return res.status(err.statusCode).json({ message: err.message });
   }
 
   req.user = user;
