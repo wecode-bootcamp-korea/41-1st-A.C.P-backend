622df4783e3bfa24205f77c20e606446f3cd390b
minkyu_lee <0106995@naver.com>
2023-01-11 22:43:46 +0900

    deleted db datas

--- db/migrations/20230104042138_create_moods_table.sql
+++
@@ -1,7 +0,0 @@
--- migrate:up
-CREATE TABLE moods (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL
-);
--- migrate:down
-DROP TABLE moods;
--- db/migrations/20230104045907_create_nutrient_types_table.sql
+++
@@ -1,9 +0,0 @@
--- migrate:up
-CREATE TABLE nutrient_types (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL,
-  created_at TIMESTAMP NOT NULL DEFAULT NOW()
-);
-
--- migrate:down
-DROP TABLE nutrient_types;
