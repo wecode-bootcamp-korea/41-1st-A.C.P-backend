6ed4e3f8ce97c73cb31c5cf775214002766b5c60
Kwang-Hwi Kim <kkh940227@gmail.com>
2023-01-12 16:39:08 +0900

    MODIFY:plantDao.js

--- models/plantDao.js
+++ models/plantDao.js
@@ -40,39 +40,28 @@ const plantsList = async (sort, offset, limit) => {
     nameDESC: "plants.name DESC",
   });
 
-  const queryRunner = appDataSource.createQueryRunner();
-  await queryRunner.connect();
-  await queryRunner.startTransaction();
+  const plantsList = await queryRunner.query(
+    `SELECT SQL_CALC_FOUND_ROWS
+      plants.id AS plant_id,
+      plants.name AS plant_name,
+      plants.price as plant_price,
+      JSON_ARRAYAGG(
+        JSON_OBJECT(
+          'url', plant_images.img_url
+        )
+      ) as plant_img,
+      plants.created_at as plant_created_at
+    FROM plants
+    JOIN plant_images ON plants.id = plant_images.plant_id
+    GROUP BY plants.id
+    ORDER BY ${sortMethod[sort]}
+    LIMIT ${limit} OFFSET ${offset}`
+  );
 
-  try {
-    const plantsList = await queryRunner.query(
-      `SELECT SQL_CALC_FOUND_ROWS
-        plants.id AS plant_id,
-        plants.name AS plant_name,
-        plants.price as plant_price,
-        JSON_ARRAYAGG(
-          JSON_OBJECT(
-            'url', plant_images.img_url
-          )
-        ) as plant_img,
-        plants.created_at as plant_created_at
-      FROM plants
-      JOIN plant_images ON plants.id = plant_images.plant_id
-      GROUP BY plants.id
-      ORDER BY ${sortMethod[sort]}
-      LIMIT ${limit} OFFSET ${offset}`
-    );
-
-    const [totalCount] = await queryRunner.query(
-      `SELECT FOUND_ROWS() AS totalCount`
-    );
-
-    return { plantsList, totalCount };
-  } catch (err) {
-    await queryRunner.rollbackTransaction();
-  } finally {
-    await queryRunner.release();
-  }
+  const [totalCount] = await queryRunner.query(
+    `SELECT FOUND_ROWS() AS totalCount`
+  );
+  return { plantsList, totalCount };
 };
 
 module.exports = {
