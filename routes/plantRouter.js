9ebb1f039eb9b0deb77e6891e0dd7d89e5e55449
Kwang-Hwi Kim <kkh940227@gmail.com>
2023-01-12 16:45:28 +0900

    MODIFY: plantRouter.js

--- routes/plantRouter.js
+++ routes/plantRouter.js
@@ -3,7 +3,7 @@ const plantController = require("../controllers/plantController");
 
 const router = express.Router();
 
-router.post("/:plantId", plantController.getPlantInfo);
+router.get("/:plantId", plantController.getPlantInfo);
 router.get("/main", plantController.plantsList);
 
 module.exports = {
