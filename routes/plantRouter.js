0fb360cd4f08deaeae977d642cf3787a7240dd87
Kwang-Hwi Kim <kkh940227@gmail.com>
2023-01-12 15:55:10 +0900

    wip

--- routes/plantRouter.js
+++ routes/plantRouter.js
@@ -3,7 +3,7 @@ const plantController = require("../controllers/plantController");
 
 const router = express.Router();
 
-router.post("/main", plantController.plantsList);
+router.get("/main", plantController.plantsList);
 
 module.exports = {
   router,
