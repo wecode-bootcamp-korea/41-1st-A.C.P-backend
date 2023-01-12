997fa8b9c95044d9c1b7dde1348d9aefdc8b6cb5
minkyu_lee <0106995@naver.com>
2023-01-12 16:08:35 +0900

    wip

--- db/migrations/20230104042112_create_species_table.sql
+++
@@ -1,8 +0,0 @@
--- migrate:up
-CREATE TABLE species (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL,
-  CONSTRAINT species_name_ukey UNIQUE (name)
-);
--- migrate:down
-DROP TABLE species;
--- db/migrations/20230111081232_create_pot_sizes.sql
+++
@@ -1,7 +0,0 @@
--- migrate:up
-CREATE TABLE pot_sizes (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL
-);
--- migrate:down
-DROP TABLE pot_sizes;
